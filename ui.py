b9e42cfc0a9a75bdcef36980f0feadadbfe41d3c
geor <georsonofzoltan@gmail.com>
2017-01-24 21:55:15 +0100

    added .gitignore

--- ui.py
+++ ui.py
@@ -97,7 +97,6 @@ class Board:
 
                         if w-self.wstep/3 < pos[0] < w+self.wstep/3 and h-self.hstep/3 < pos[1] < h+self.hstep/3:
                             self.set_pawn(coords)
-                            #bullshitaasdfasdf
                             # reply
                             self.play_move()
                             print "ok"
