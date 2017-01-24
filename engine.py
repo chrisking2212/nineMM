6fc563f9d8acc4deda7992dcd477f5279e1678c1
Christian <cmoehle@gmx.net>
2017-01-24 21:34:31 +0100

    Engine code shortened + engine is working pretty well

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def count_threesome(color, board):
+
+def count_3s(color, board):
 
     count_r = 0
     count_c = 0
@@ -11,50 +12,43 @@ def count_threesome(color, board):
     for i in xrange(0, 7):
         if i != 3:
             for j in xrange(0, 7):
-                if board[i, j] != 3:
-                    if board[i, j] == color:
-                        sum_r[i] += 1
+                if board[i, j] == color:
+                    sum_r[i] += 1
+                if board[j, i] == color:
+                    sum_c[i] += 1
             if sum_r[i] == 3:
                 count_r += 1
+            if sum_c[i] ==3:
+                count_c += 1
         else:
             for j in xrange(0, 3):
                 if board[i, j] == color:
                     sum_r[i] += 1
+                if board[j, i] == color:
+                    sum_c[i] += 1
             if sum_r[i] == 3:
                 count_r += 1
             sum_r[i] = 0
+            if sum_c[i] == 3:
+                count_c += 1
+            sum_c[i] = 0
             for j in xrange(4, 7):
                 if board[i, j] == color:
                     sum_r[i] += 1
+                if board[j, i] == color:
+                    sum_c[i] += 1
             if sum_r[i] == 3:
                 count_r += 1
-
-    print "row 3s: "+str(count_r)
-
-    for j in xrange(0, 7):
-        if j != 3:
-            for i in xrange(0, 7):
-                if board[i, j] != 3:
-                    if board[i, j] == color:
-                        sum_c[j] += 1
-            if sum_c[j] == 3:
-                count_c += 1
-        else:
-            for i in xrange(0, 3):
-                if board[i, j] == color:
-                        sum_c[j] += 1
-            if sum_c[j] == 3:
-                count_c += 1
-            sum_c[j] = 0
-            for i in xrange(4, 7):
-                if board[i, j] == color:
-                    sum_c[j] += 1
-            if sum_c[j] == 3:
+            if sum_c[i] == 3:
                 count_c += 1
 
-    #print count_c
-    total_threesome = count_r+count_c
-    print "total threesomes: "+str(total_threesome)
+    total_3s = count_r + count_c
+
+    #print "row 3s: "+str(count_r)
+    #print "column 3s: " + str(count_c)
+    #print "total 3s: " + str(total_3s)
+
+    return total_3s
 
 
 def evaluate(player, board):
@@ -84,14 +78,18 @@ def evaluate(player, board):
             rating = 100
 
     if rating != 100 and rating != -100:
-        count_threesome(2, board)
+        if player == 1:
+            rating = 20*(count_3s(1, board)-count_3s(2, board))
+        if player == 2:
+            rating = 20*(count_3s(2, board)-count_3s(1, board))
 
+    print "rating: " + str(rating)
     return rating
 
-board = np.matrix([[2, 3, 3, 2, 3, 3, 2],
+board = np.matrix([[0, 3, 3, 0, 3, 3, 0],
                    [3, 0, 3, 0, 3, 2, 3],
                    [3, 3, 0, 0, 1, 3, 3],
-                   [2, 2, 2, 3, 2, 2, 2],
+                   [0, 2, 2, 3, 1, 2, 0],
                    [3, 3, 0, 0, 1, 3, 3],
                    [3, 2, 3, 1, 3, 2, 3],
                    [0, 3, 3, 1, 3, 3, 0]])
